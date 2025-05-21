96a63a3e4769b5d6417de3fc9c13c6fc1e032ac5
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-05-21 21:43:49 +0530

    project initialized

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("hello from the server cool");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});

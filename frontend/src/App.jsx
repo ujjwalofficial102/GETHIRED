b2b572aa02e9f907a8b899d56faf6fdd66aa23e5
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-05-26 22:33:55 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellow-600">LOLLA</div>
+      <div className="">
+        <Button className={"cursor-pointer"}>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;

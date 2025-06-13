f5a88b3ee9e4fe8fa52bba1cb9c7c93048c98355
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-06-14 00:57:59 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 dotenv.config();
+
+const _dirname = path.resolve();
 
 app.use(
   cors({
@@ -28,6 +31,11 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
